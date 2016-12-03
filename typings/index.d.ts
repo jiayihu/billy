1144d5057188f96be7b7e042e8238dbf32884671
Jiayi Hu <jiayi.ghu@gmail.com>
2016-12-03 17:08:42 +0100

    chore: use Typescript 2 @types instead of typings

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/core-js/index.d.ts" />
-/// <reference path="globals/localforage/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/invariant/index.d.ts" />
