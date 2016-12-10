8c88bb53962eb257c7c03908b9b29d6b3bb04ae6
Jiayi Hu <jiayi.ghu@gmail.com>
2016-12-10 22:04:16 +0100

    feat(ui): add InputEdit component

---
+++ src/features/ui/input-edit/input-edit.component.ts
@@ -0,0 +1,45 @@
+import { Component, ElementRef, EventEmitter, Input, Output, QueryList, SimpleChange, ViewChildren } from '@angular/core';
+
+@Component({
+  selector: 'input-edit',
+  template: require('./input-edit.component.html'),
+  styles: [require('./input-edit.component.css')],
+})
+export default class InputEditComponent {
+  private isEditing: boolean = false;
+  private currentValue: string;
+  // @NOTE: @ViewChild returns always undefined with *ngIf
+  // @see {http://stackoverflow.com/questions/34947154/angular-2-viewchild-annotation-returns-undefined}
+  @ViewChildren('input')
+  private childQuery: QueryList<ElementRef>;
+  private inputEl: ElementRef;
+
+  @Input() initialValue: string;
+  @Input() charsLength: number = 20;
+  @Output() onChange = new EventEmitter<string>();
+
+  ngOnChanges(changes: { initialValue: SimpleChange }) {
+    if (changes.initialValue.previousValue !== changes.initialValue.currentValue) {
+      this.currentValue = changes.initialValue.currentValue;
+    }
+  }
+
+  ngAfterViewInit() {
+    this.childQuery.changes.subscribe((comps: QueryList<ElementRef>) => {
+      this.inputEl = comps.first;
+    });
+  }
+
+  ngAfterViewChecked() {
+    if (this.inputEl) this.inputEl.nativeElement.focus();
+  }
+
+  handleChange(value: string) {
+    this.isEditing = false;
+    this.onChange.emit(value);
+  }
+
+  handleStartEdit() {
+    this.isEditing = true;
+  }
+}
--- src/features/ui/ui.module.ts
+++ src/features/ui/ui.module.ts
@@ -4,11 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import BtnComponent from './button/button.component';
 import FieldComponent from './field/field.component';
+import InputEditComponent from './input-edit/input-edit.component';
 import ModalComponent from './modal/modal.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: [BtnComponent, FieldComponent, ModalComponent],
-  exports: [BtnComponent, FieldComponent, ModalComponent],
+  declarations: [BtnComponent, FieldComponent, InputEditComponent, ModalComponent],
+  exports: [BtnComponent, FieldComponent, InputEditComponent, ModalComponent],
 })
 export default class UIModule { }
