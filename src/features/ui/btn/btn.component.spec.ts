8bd4ae977b8b9e7217a99c32c57dbce15941591a
Jiayi Hu <jiayi.ghu@gmail.com>
2017-01-07 13:08:58 +0100

    test: use createTestComponent util

---
+++ src/features/ui/btn/btn.component.spec.ts
@@ -0,0 +1,43 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Component, DebugElement } from '@angular/core';
+import createTestComponent from '@utils/create-test-component';
+import BtnComponent from './btn.component';
+
+@Component({
+  selector: 'test-cmp',
+  template: '',
+})
+class TestComponent {
+  event: any;
+
+  handleClick(event) { this.event = event; }
+}
+
+describe('BtnComponent', () => {
+  let comp: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugEl: DebugElement;
+  let el: HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [BtnComponent, TestComponent],
+    });
+    const template = '<btn type="primary" (onClick)="handleClick($event)"></btn>';
+    fixture = createTestComponent(template, TestComponent);
+    comp = fixture.componentInstance;
+    debugEl = fixture.debugElement.query(By.css('button'));
+    el = debugEl.nativeElement;
+    fixture.detectChanges();
+  });
+
+  it('should display the correct btn type', () => {
+    expect(el.className).toEqual('btn btn-primary');
+  });
+
+  it('should pass the Event instance when clicked', () => {
+    debugEl.triggerEventHandler('click', { type: 'click' });
+    expect(comp.event).toEqual({ type: 'click' });
+  });
+});
---
+++ src/utils/create-test-component.ts
@@ -0,0 +1,11 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
+export default function createTestComponent<T>(
+  html: string,
+  type: {new (...args: any[]): T},
+): ComponentFixture<T> {
+  TestBed.overrideComponent(type, { set: { template: html } });
+  const fixture = TestBed.createComponent(type);
+  fixture.detectChanges();
+  return fixture as ComponentFixture<T>;
+}
